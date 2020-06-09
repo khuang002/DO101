12a783a036eb15bfec53157dd83f099f194018b3
Kun <khuang002>
2020-06-09 16:10:57 +0800

    add end points / and /Mars

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Hello World!\n");
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+    res.send("Hello World!\n");
+});
+
+app.get('/mars', function(req, res){
+    res.send("Hello Mars!\n");
+});
+
+app.listen(3000, function(){
+    console.log("example app listening on port 3000!")
+});
